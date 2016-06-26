cc8876145fc7c61b174c386749887a4463cdd89b
jgw4sq <jgw4sq@virginia.edu>
2016-06-26 12:24:20 -0400

    'commit'

---
+++ src/main/java/com/weber/MySchedule.java
@@ -0,0 +1,58 @@
+package com.weber;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MySchedule
+ */
+@WebServlet("/MySchedule")
+public class MySchedule extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MySchedule() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		try{
+		
+		request.setAttribute("shifts", Login.USER.getShifts());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		PrintWriter writer = response.getWriter();
+		writer.println("MySchedule");
+        request.getRequestDispatcher("/WEB-INF/myschedule.jsp").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/hompage.jsp
@@ -0,0 +1,9 @@
+<html>
+<h3>This is the Homepage</h3>
+
+<a href = "/Testwebapp/Login">Logout </a>
+<a href = "/Testwebapp/MyAccount">MyAccount </a>
+<a href = "/Testwebapp/MySchedule">MySchedule </a>
+
+
+</html>
