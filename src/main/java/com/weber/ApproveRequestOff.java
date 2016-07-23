9897edbea1f01cf73cf4188941b6f216d1555771
jgw4sq <jgw4sq@virginia.edu>
2016-07-22 22:21:53 -0400

    'commit'

--- src/main/java/com/weber/ApproveRequestOff.java
+++ src/main/java/com/weber/ApproveRequestOff.java
@@ -56,7 +56,7 @@ public class ApproveRequestOff extends HttpServlet {
 			while(rs.next()){
 				String guard=rs.getString("guard");
 				int id= rs.getInt("id");
-				System.out.println(rs.getTimestamp("startTime"));
+				System.out.println(rs.getTimestamp("endTime"));
 				Timestamp startTime=rs.getTimestamp("startTime");
 				Timestamp endTime=rs.getTimestamp("endTime");
 				boolean approved=rs.getBoolean("approved");
