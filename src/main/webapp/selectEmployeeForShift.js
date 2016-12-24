549cdb13a2abe94392fac3a9572e426015706b7d
jgw4sq <jgw4sq@virginia.edu>
2016-12-24 12:50:22 -0500

    commit

--- src/main/webapp/selectEmployeeForShift.js
+++ src/main/webapp/selectEmployeeForShift.js
@@ -4,10 +4,17 @@
 function changeEmployeeValue(newValue){
 	alert("hi");
 	if(!newValue=="Select Other Option to Change Employee"){
+		alert("passed first");
 		if(!newValue=="There are no other avilable employees"){
+			alert("passed second");
 			document.getElementById("employee").value = newValue;
+		}else{
+			alert("failed second");
 		}
+	}else{
+
+		alert("failed first");
 	}
 	
 
-};
+}
