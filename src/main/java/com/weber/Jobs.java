8971b4df4889e9f197925747e1efd68a54e46071
John Weber <jgw4sq@virginia.edu>
2017-04-06 14:38:42 -0400

    adding asyn background job

--- src/main/java/com/weber/Jobs.java
+++ src/main/java/com/weber/Jobs.java
@@ -1,4 +1,12 @@
 package job;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+
+import javax.ws.rs.core.Response;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,12 +16,56 @@ public class Jobs implements Job {
 	int x =0;
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
-			updateData();
+			try {
+				getNBA();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		
+		
+		
+		updateData();
 	}
 	
 	public void updateData(){
 		System.out.println(x);
 		x+=1;
 	}
+	
+	
+	public static void getNBA() throws Exception{
+		Date date = new Date();
+		String dateString = date.getYear()+1900+""+String.format("%02d",date.getMonth()+1)+String.format("%02d",date.getDate());
+		String fullHtml= getHTML("http://www.espn.com/nba/scoreboard");
+		String [] split = fullHtml.split(";window.espn.scoreboardSettings");
+		//System.out.println("Split 0: "+split[0]);
+		String [] goodStuff = split[0].split("window.espn.scoreboardData 	=");
+		//System.out.println("Length: "+goodStuff.length);
+		String actualGoodStuff = goodStuff[1].trim();
+		System.out.println(actualGoodStuff);
+	}
+	
+	
+	public static String getHTML(String urlToRead) throws Exception {
+	    StringBuilder result = new StringBuilder();
+	    URL url = new URL(urlToRead);
+	    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	    conn.setRequestMethod("GET");
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	    String line;
+
+	    while ((line = rd.readLine()) != null) {
+	  	  //System.out.println("hello");
+	  	  if(line.contains("window.espn.scoreboardData")){
+	  		 // System.out.println(line);
+		         result.append(line);
+
+	  	  }
+	    }
+	    rd.close();
+	    return result.toString();
+	 }
 
 }
