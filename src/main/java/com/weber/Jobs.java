03a64c9ad1cd0b2daf7765d15479c3ca8b9aad6a
John Weber <jgw4sq@virginia.edu>
2017-04-06 16:01:20 -0400

    adding asyn background job

--- src/main/java/com/weber/Jobs.java
+++ src/main/java/com/weber/Jobs.java
@@ -28,6 +28,7 @@ public class Jobs implements Job {
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 			try {
 				getNBA();
+				getMLB();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -49,6 +50,151 @@ public class Jobs implements Job {
 		Date date = new Date();
 		String dateString = date.getYear()+1900+""+String.format("%02d",date.getMonth()+1)+String.format("%02d",date.getDate());
 		String fullHtml= getHTML("http://www.espn.com/nba/scoreboard");
+		String [] split = fullHtml.split(";window.espn.scoreboardSettings");
+		//System.out.println("Split 0: "+split[0]);
+		String [] goodStuff = split[0].split("window.espn.scoreboardData 	=");
+		//System.out.println("Length: "+goodStuff.length);
+		String actualGoodStuff = goodStuff[1].trim();
+		System.out.println(actualGoodStuff);
+		try {
+			JSONObject jsonObject = new JSONObject(actualGoodStuff);
+			//System.out.println(jsonObject.getJSONArray("leagues"));
+			JSONArray events = jsonObject.getJSONArray("events");
+			//System.out.println(events.toString());
+			ArrayList<Competition> games = new ArrayList<Competition>();
+			for (int i = 0; i < events.length(); i++) {
+				Competition game = new Competition();
+				//System.out.println(events.get(i));
+				JSONObject event = new JSONObject(events.get(i).toString());
+				//System.out.println("Event: "+event.toString());
+				//System.out.println(event.toString());
+				game.setId(Integer.parseInt(event.getString("id")));
+				JSONArray competitions = event.getJSONArray("competitions");
+				for (int j = 0; j < competitions.length(); j++) {
+
+					JSONObject competition = new JSONObject(competitions.get(j).toString());
+					System.out.println(competition.toString());
+					if (competition.has("odds")) {
+						JSONArray odds = competition.getJSONArray("odds");
+						if (odds.length() >= 1) {
+							JSONObject odd = new JSONObject(odds.get(0).toString());
+							if(odd.has("details")){
+							game.setSpread(odd.getString("details"));
+							}
+							if(odd.has("overUnder")){
+							game.setOverUnder((double)(odd.getDouble(("overUnder"))));
+							}
+
+						}
+
+					}
+					JSONObject status = competition.getJSONObject("status");
+					int period = status.getInt("period");
+					String displayClock = status.getString("displayClock");
+					JSONObject type = status.getJSONObject("type");
+					boolean completed = type.getBoolean("completed");
+					game.setPeriod(period);
+					game.setDisplayclock(displayClock);
+					game.setCompleted(completed);
+					String dateOfComp = type.getString("shortDetail");
+					game.setGameTime(dateOfComp);
+
+
+					JSONArray competitors = competition.getJSONArray("competitors");
+					for (int k = 0; k < competitors.length(); k++) {
+						Competitor competitorToAdd = new Competitor();
+						JSONObject competitor = new JSONObject(competitors.get(k).toString());
+						JSONObject team = competitor.getJSONObject("team");
+						JSONArray records = competitor.getJSONArray("records");
+						for (int z = 0; z < records.length(); z++) {
+							JSONObject record = new JSONObject(records.get(z).toString());
+							String recordString = record.getString("summary");
+							String recordType = record.getString("type");
+							if (recordType.equals("total")) {
+								competitorToAdd.setTotalRecord(recordString);
+							} else if (recordType.equals("home")) {
+								competitorToAdd.setHomeRecord(recordString);
+							} else if (recordType.equals("road")) {
+								competitorToAdd.setAwayRecord(recordString);
+							}
+						}
+						String name = team.getString("displayName");
+						competitorToAdd.setName(name);
+						competitorToAdd.setScore(Double.parseDouble(competitor.getString("score")));
+						competitorToAdd.setAbbreviation(team.getString("abbreviation"));
+						//System.out.println(name);
+						if (competitor.getString("homeAway").equals("home")) {
+							game.setHomeCompetitor(competitorToAdd);
+						} else {
+							game.setAwayCompetitor(competitorToAdd);
+						}
+					}
+				}
+				
+
+				games.add(game);
+
+
+			}
+			Statement stmt =null;
+			
+			Class.forName("com.mysql.jdbc.Driver");  
+			 Connection con=DriverManager.getConnection(  
+						"jdbc:mysql://127.9.167.130:3306/jake","adminnHxi4B8","fWUk7PSKVlcV");
+				stmt = con.createStatement();
+				for( int index=0; index<games.size(); index++){
+					Competition comp = games.get(index);
+					System.out.println("Checking: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
+
+					String sql= "SELECT COUNT(id) as total FROM Games WHERE id="+comp.getId()+";";
+					stmt= con.createStatement();
+					ResultSet rs = stmt.executeQuery(sql);
+					int total=0;
+					if(rs.next()){
+					 total = rs.getInt("total");
+					}
+					
+					if(total==0){
+						System.out.println("Adding: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
+
+						sql = "INSERT INTO Games (homeTeam,awayTeam,id,spread,completed,date,displayClock,homeTeamScore,awayTeamScore,gameTime) VALUES ('"+comp.getHomeCompetitor().getName()+"','"+comp.getAwayCompetitor().getName()+"',"+comp.getId()+",'"+comp.getSpread()+"',"+comp.isCompleted()+",'"+comp.getDate()+"','"+comp.getDisplayclock()+"',"+comp.getHomeCompetitor().getScore()+","+comp.getAwayCompetitor().getScore()+",'"+comp.getGameTime()+"')";
+						stmt.execute(sql);
+						System.out.println("Added: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
+
+					}else{
+						System.out.println("Updating: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
+
+						sql = "UPDATE Games SET completed="+comp.isCompleted()+", displayClock='"+comp.getDisplayclock()+"', homeTeamScore="+comp.getHomeCompetitor().getScore()+", awayTeamScore="+comp.getAwayCompetitor().getScore()+", gameTime='"+comp.getGameTime()+"' WHERE id="+comp.getId();
+					
+						stmt.execute(sql);
+						System.out.println("Updated: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
+
+					}
+					
+					
+					
+					
+				}
+				
+				stmt.close();
+				
+				//stmt= con.createStatement();
+				//sql ="UPDATE SHIFTS SET guard=null , email=null WHERE guard='"+firstname+" "+lastname+"';";
+				 //rs = stmt.executeUpdate(sql);
+				 //stmt.close();
+				con.close();
+
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void getMLB() throws Exception{
+		Date date = new Date();
+		String dateString = date.getYear()+1900+""+String.format("%02d",date.getMonth()+1)+String.format("%02d",date.getDate());
+		String fullHtml= getHTML("http://www.espn.com/mlb/scoreboard");
 		String [] split = fullHtml.split(";window.espn.scoreboardSettings");
 		//System.out.println("Split 0: "+split[0]);
 		String [] goodStuff = split[0].split("window.espn.scoreboardData 	=");
