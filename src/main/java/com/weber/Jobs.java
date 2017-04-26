6be75e5ec516de2d6e45edf28b891bb8d3a6f1a3
John Weber <jgw4sq@virginia.edu>
2017-04-26 14:25:38 -0400

    update bets job

--- src/main/java/com/weber/Jobs.java
+++ src/main/java/com/weber/Jobs.java
@@ -403,7 +403,7 @@ public class Jobs implements Job {
 							if(odd.has("details")){
 							game.setSpread(odd.getString("details"));
 
-							System.out.println("game spread: "+ game.getSpread());
+							//System.out.println("game spread: "+ game.getSpread());
 							}
 							if(odd.has("overUnder")){
 							game.setOverUnder((double)(odd.getDouble(("overUnder"))));
@@ -487,6 +487,7 @@ public class Jobs implements Job {
 					
 					if(total==0){
 						//System.out.println("Adding: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
+						System.out.println(comp.getSpread());
 
 						sql = "INSERT INTO Games (homeTeam,awayTeam,id,spread,completed,date,displayClock,homeTeamScore,awayTeamScore,gameTime,homeTeamAbbreviation,awayTeamAbbreviation,started,homeTeamTotalRecord,homeTeamHomeRecord,homeTeamAwayRecord,awayTeamTotalRecord,awayTeamHomeRecord,awayTeamAwayRecord,gameType) VALUES ('"+comp.getHomeCompetitor().getName()+"','"+comp.getAwayCompetitor().getName()+"',"+comp.getId()+",'"+comp.getSpread()+"',"+comp.isCompleted()+",'"+comp.getDate()+"','"+comp.getDisplayclock()+"',"+comp.getHomeCompetitor().getScore()+","+comp.getAwayCompetitor().getScore()+",'"+comp.getGameTime()+"','"+comp.getHomeCompetitor().getAbbreviation()+"','"+comp.getAwayCompetitor().getAbbreviation()+"',"+comp.isStarted()+",'"+comp.getHomeCompetitor().getTotalRecord()+"','"+comp.getHomeCompetitor().getHomeRecord()+"','"+comp.getHomeCompetitor().getAwayRecord()+"','"+comp.getAwayCompetitor().getTotalRecord()+"','"+comp.getAwayCompetitor().getHomeRecord()+"','"+comp.getAwayCompetitor().getAwayRecord()+"','NBA')";
 						stmt.execute(sql);
@@ -561,7 +562,7 @@ public class Jobs implements Job {
 							if(odd.has("details")){
 								//System.out.println("getting mlb odds");
 							game.setSpread(odd.getString("details"));
-							System.out.println("game spread: "+ game.getSpread());
+							//System.out.println("game spread: "+ game.getSpread());
 							}
 							if(odd.has("overUnder")){
 							game.setOverUnder((double)(odd.getDouble(("overUnder"))));
@@ -650,7 +651,7 @@ public class Jobs implements Job {
 					
 					if(total==0){
 //						System.out.println("Adding: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
-
+						System.out.println(comp.getSpread());
 						sql = "INSERT INTO Games (homeTeam,awayTeam,id,spread,completed,date,displayClock,homeTeamScore,awayTeamScore,gameTime,homeTeamAbbreviation,awayTeamAbbreviation,started,homeTeamTotalRecord,homeTeamHomeRecord,homeTeamAwayRecord,awayTeamTotalRecord,awayTeamHomeRecord,awayTeamAwayRecord,gameType) VALUES ('"+comp.getHomeCompetitor().getName()+"','"+comp.getAwayCompetitor().getName()+"',"+comp.getId()+",'"+comp.getSpread()+"',"+comp.isCompleted()+",'"+comp.getDate()+"','"+comp.getDisplayclock()+"',"+comp.getHomeCompetitor().getScore()+","+comp.getAwayCompetitor().getScore()+",'"+comp.getGameTime()+"','"+comp.getHomeCompetitor().getAbbreviation()+"','"+comp.getAwayCompetitor().getAbbreviation()+"',"+comp.isStarted()+",'"+comp.getHomeCompetitor().getTotalRecord()+"','"+comp.getHomeCompetitor().getHomeRecord()+"','"+comp.getHomeCompetitor().getAwayRecord()+"','"+comp.getAwayCompetitor().getTotalRecord()+"','"+comp.getAwayCompetitor().getHomeRecord()+"','"+comp.getAwayCompetitor().getAwayRecord()+"','MLB')";
 						stmt.execute(sql);
 						//System.out.println("Added: "+comp.getHomeCompetitor().getName()+" vs. "+comp.getAwayCompetitor().getName());
