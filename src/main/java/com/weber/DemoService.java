316ea2075195610d383d7e5b50ca0387efb6204a
John Weber <jgw4sq@virginia.edu>
2017-04-27 22:36:49 -0400

    added creategame for demo service

--- src/main/java/com/weber/DemoService.java
+++ src/main/java/com/weber/DemoService.java
@@ -112,6 +112,17 @@ public class DemoService {
 				
 				return Response.status(200).entity(object.toString()).build();
 				
+			}else if(instruction.equals("zeroOutUser")){
+				sql="UPDATE STOCKUSERS set accountBalance=0, availableBalance=0, usedBalance=0 WHERE email=gradedemo@gmail.com";
+				stmt.execute(sql);
+				sql="UPDATE STOCKUSERS set accountBalance=0, availableBalance=0, usedBalance=0 WHERE email=classdemo@gmail.com";
+				stmt.execute(sql);
+				sql="UPDATE STOCKUSERS set accountBalance=0, availableBalance=0, usedBalance=0 WHERE email=wx";
+				stmt.execute(sql);
+				object.put("result", "success");
+				
+				return Response.status(200).entity(object.toString()).build();
+				
 			}
 					
 		
